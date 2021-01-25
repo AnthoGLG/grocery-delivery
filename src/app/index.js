7f7fa7bcacd0a43c9dd7ce5dffa1d5b6e544b197
Anthony Gallego <3igweb@gmail.com>
2021-01-25 13:07:51 +0100

    Bootstrap4 Grid

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,31 @@
-import React, { Fragment } from 'react';
-import { Navbar } from './components';
-import './styles/App.css'
+import React, { Fragment } from "react";
+import { Navbar } from "./components";
+import "./styles/App.css";
 
+const SideMenu = () => {
+  const links = ["Légumes", "Fruits", "Produits Frais", "Epicerie", "Boissons"];
+  return (
+    <div className="col-sm-2 sidebar">
+      <ul>
+        {links.map((link) => {
+          return <li>{link}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Fragment>
-      <Navbar  />
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <SideMenu />
+          <div className="col-sm">Store</div>
+        </div>
+      </div>
     </Fragment>
   );
-}
+};
 export default App;
