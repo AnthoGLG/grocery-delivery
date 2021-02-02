aec16b61cb774bc2a2285223579665a86088fa9b
Anthony Gallego <3igweb@gmail.com>
2021-02-02 12:10:51 +0100

    Mise en page de la View Cart(Part 2 :Table et Row)

--- src/app/views/Cart.js
+++ src/app/views/Cart.js
@@ -1,13 +1,71 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "../styles/App.css";
 
+const Row = () => {
+  return (
+    <tr>
+      <td>
+        <img
+          width="70"
+          height="70"
+          src={process.env.PUBLIC_URL + `/assets/0/citron.png`}
+          alt="citrons"
+        />
+      </td>
+      <td>ref</td>
+      <td>€0.00</td>
+      <td>
+        <div className="btn-group" role="group" aria-label="Basic example">
+          <button
+            type="button"
+            className="btn btn-secondary">
+            -
+          </button>
+          <span className="btn btn-light">1</span>
+          <button
+            type="button"
+            className="btn btn-secondary">
+            +
+          </button>
+        </div>
+      </td>
+      <td>€2.99</td>
+      <td>
+        <button
+          type="button"
+          className="btn btn-danger remove">
+          X
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+
+const Table = () => {
+  return (
+    <table>
+      <tr>
+        <th width="200">Product</th>
+        <th width="80">Reference</th>
+        <th width="150">Price</th>
+        <th width="150">Quantity</th>
+        <th width="200">Total</th>
+      </tr>
+      <Row />
+      <Row />
+    </table>
+  );
+};
 
 export const CartPage = () => {
-    return (
-      <Fragment>
+  return (
+    <Fragment>
+      <div className="container">
         <div className="row">
-          <div className="col-sm-2 cart">
+          <div className="col-sm cart">
+            <Table />
           </div>
           <div className="col-sm-3 order-summary">
             <ul className="list-group">
@@ -47,6 +105,7 @@ export const CartPage = () => {
             </button>
           </div>
         </div>
-      </Fragment>
-    );
-}
+      </div>
+    </Fragment>
+  );
+};
