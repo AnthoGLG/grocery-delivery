5d9a166369f36d9946811ff0c64fc19b707f32ba
Anthony Gallego <3igweb@gmail.com>
2021-02-05 12:09:05 +0100

    definir les actions qui vont servir a dispatcher le type d'action accompagner du payload (modifier)

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,37 +1,40 @@
+/*
+ * actions types
+ */
+export const actions = {
+  GET_ITEMS: "GET_ITEMS",
+  ADD_TO_CART: "ADD_TO_CART",
+  UPDATE_CART: "UPDATE_CART",
+  REMOVE_FROM_CART: "REMOVE_FROM_CART",
+  SAVE_CART: "SAVE_CART",
+};
 
 /*
-* actions types
-*/
-
-const actions = {
-    ADD_TO_CART = "ADD_TO_CART",
-    UPDATE_CART ="UPDATE_CART",
-    REMOVE_FROM_CART= "REMOVE_FROM_CART"
-}
-
-/*
-* actions creators
-*definir les fonctions qui vont servir a dispatcher le type d'action accompagner du payload
-*/
-
-const uid = Math.random().toString(34).slice(2)
-function addToCart(item, quantity) {
-    return{
-        type: actions.ADD_TO_CART,
-        payload : {id: uid(), quantity: quantity, details: item} 
-    }
-}
-
-function updateCart(item, quantity) {
-    return {
-        type : actions.UPDATE_CART,
-        payload : {item: item, quantity}
-    }
-}
-
-function removeFromCart(item) {
-    return {
-        type: actions.REMOVE_FROM_CART,
-        payload : item
-    }
-}
+ * actions creators
+ *definir les fonctions qui vont servir a dispatcher le type d'action accompagner du payload
+ */
+const uid = () => Math.random().toString(34).slice(2);
+export const addToCart = (item, quantity) => {
+  return {
+    type: actions.ADD_TO_CART,
+    payload: { id: uid(), quantity: quantity, details: item },
+  };
+};
+export const updateCart = (item, quantity) => {
+  return {
+    type: actions.UPDATE_CART,
+    payload: { item: item, quantity: quantity },
+  };
+};
+export const removeFromCart = (item) => {
+  return {
+    type: actions.REMOVE_FROM_CART,
+    payload: item,
+  };
+};
+export const saveCart = (items) => {
+  return {
+    type: actions.SAVE_CART,
+    payload: { items: items },
+  };
+};
