eb91e2057e626681af4408e11f074508c6acd100
Anthony Gallego <3igweb@gmail.com>
2021-02-05 13:31:28 +0100

    creer le store pour acceder à tous les composants depuis l'application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { addToCart } from "./app/lib/actions";
+import { store } from "./app/lib/store"
 import App from './app/views'
 
-ReactDOM.render(<App />, document.getElementById("root")
+// Log the initial state
+console.log(store.getState);
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+store.dispatch(addToCart({name: "citron"}, 2))
+store.dispatch(addToCart({name: "kiwi"}, 5))
+unsubscribe()
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById("root")
 );
 
